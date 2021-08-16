3d2185ed58224cc3714b1f79cf6b5978669203df
Mahendra  Chaudhari <43503477+MahendraCloudUseKaro@users.noreply.github.com>
2021-08-16 17:12:48 +0530

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -9,7 +9,7 @@ public class Example {
 
     @RequestMapping("/")
     String home() {
-        return "Hello Mahendraa !";
+        return "Hello Mahendraaaa !";
     }
 
     public static void main(String[] args) throws Exception {
